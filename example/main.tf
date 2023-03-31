2fdb67e9d00c5dfe52201b6913b7042075f7f2f0
Jonathan Stairs <jonathan.stairs@rewind.io>
2023-03-31 08:35:36 -0400

    fix(): Update to use template_file command
    Signed-off-by: Jonathan Stairs <jonathan.stairs@rewind.io>

--- example/main.tf
+++ example/main.tf
@@ -9,7 +9,7 @@ module "cdn" {
   stage                    = "prod"
   name                     = "app"
   aliases                  = ["assets.cloudposse.com"]
-  parent_zone_id           = "${aws_route53_zone.primary.zone_id}"
+  parent_zone_id           = aws_route53_zone.primary.zone_id
   use_regional_s3_endpoint = "true"
   origin_force_destroy     = "true"
   cors_allowed_headers     = ["*"]
@@ -19,9 +19,9 @@ module "cdn" {
 }
 
 resource "aws_s3_bucket_object" "index" {
-  bucket       = "${module.cdn.s3_bucket}"
+  bucket       = module.cdn.s3_bucket
   key          = "index.html"
   source       = "${path.module}/index.html"
   content_type = "text/html"
-  etag         = "${md5(file("${path.module}/index.html"))}"
+  etag         = md5(file("${path.module}/index.html"))
 }
